40e71b29bdb9364f778d0f01fdd3f91e63db916f
andy <andyggggg>
2024-02-11 22:28:09 +0300

    Добавлено сравнение чиселЭ

--- les1/Task1/Program.cs
+++ les1/Task1/Program.cs
@@ -12,4 +12,32 @@ double sum = (a1 * b1) / (c1 + d1);
 
 Console.WriteLine(sum);
 
+Console.WriteLine("ВВедите первое число");
+int FirstNumber =int.Parse(Console.ReadLine());
+Console.WriteLine("ВВедите второе число");
+int SecondNumber =int.Parse(Console.ReadLine());
+int HightNumber = 0;
+int SmallNumber = 0;
+ 
+    if (FirstNumber > SecondNumber) {
+    HightNumber = FirstNumber;
+    SmallNumber = SecondNumber;
+     Console.WriteLine("Большее число " + HightNumber);
+    Console.WriteLine("Меньшее число " + SmallNumber);
+    }
+ 
+    else if (FirstNumber < SecondNumber) {
+           
+        HightNumber = SecondNumber;
+        SmallNumber = FirstNumber;
+        
+    Console.WriteLine("Большее число " + HightNumber);
+    Console.WriteLine("Меньшее число " + SmallNumber);
+    }
+    else {
+        Console.WriteLine("Числа ровны");
+        
+    }
 
+
+   
--- les1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
+++ les1/Task1/obj/Debug/net8.0/Task1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ee09afd371be902bbb732ead176dc79a42c22d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d3c89ab3b833f4e500293229eb76e9f8cbd18f91")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
